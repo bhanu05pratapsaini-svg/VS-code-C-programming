56453a58676edc27262007952d7cd99c3841c206
bhanu05pratapsaini-svg <bhanu05pratapsaini@gmail.com>
2025-08-26 21:19:42 +0530

    Add files via upload

---
+++ code5.c
@@ -0,0 +1,18 @@
+// Q5 write a program to convert temperature from celsius to fahrenheit.
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    
+    scanf("%f", &celsius);
+
+    
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    
+    printf("Fahrenheit=%.0f\n", fahrenheit);
+
+    return 0;
+}
+
