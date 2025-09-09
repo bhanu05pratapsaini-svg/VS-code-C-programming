36d6f6892053c6a5fc658f0eae06133997582d72
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-09-10 01:26:46 +0530

    Pattern Q20

---
+++ Pattern20.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+   int space ;
+    space = 18; 
+    for (i = 1; i <= 10; i++) {
+        for (j = 0; j < i; j++) {
+            printf("*");
+        }
+        for (j = 0; j < space; j++) {
+            printf(" ");
+        }
+        for (j = 0; j < i; j++) {
+            printf("*");
+        }
+        space -= 2;
+        printf("\n");
+    }
+      space = 0;
+         for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10 - i; j++) {
+            printf("*");
+        }
+       for (j = 0; j < space; j++) {
+            printf(" ");
+        }
+        for (j = 0; j < 10 - i; j++) {
+            printf("*");
+        }
+        space += 2;
+        printf("\n");
+    }
+    return 0;
+}
