9be74f8571cb0b4860c43af1dacca2c0081f5cde
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-09-09 23:19:49 +0530

    Pattern Q11

--- Pattern11.c
+++ Pattern11.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int rows = 5;
     int i, j;
-
-    for (i = 1; i <= rows; i++) {
-        for (j = 1; j <= i; j++) {
-            
-            if (j % 2 == 1)
-                printf("1 ");
-            else
-                printf("0 ");
-        }
-        printf("\n");
-    }
+    int start = 1;
+    for(i =0; i <= 5; i++)  {     
+  {    if(i%2==0) 
+            start=1;
+        else 
+            start=0;
+        for(j=0;j<=i;j++)
+        {  printf("%d",start);
+           start = 1-start;   }
+        }   
+        printf("\n");}
 
     return 0;
 }
-
