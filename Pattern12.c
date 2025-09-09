6335b2d71e1381cc1f39ffdf473a25d9cb8d4584
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-09-09 22:32:20 +0530

    Pattern Q12

--- Pattern12.c
+++ Pattern12.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, space;
-    int rows = 4;
-
-    for (i = 1; i <= rows; i++) {
-        
-        for (j = 1; j <= i; j++) {
-            printf("%d", j);
-        }
-
-        space = 2 * (rows - i);
-        for (j = 1; j <= space; j++) {
-            printf(" ");
-        }
-        for (j = i; j >= 1; j--) {
-            printf("%d", j);
-        }
-
-        printf("\n");
-    }
-
+    int space =6;
+    int i,j;
+    for(i = 1; i <= 4; i++)       
+{        for(j = 1; j <=i; j++) 
+             {printf("%d",j);}     
+         for(j= 1; j <=space ; j++) 
+             {printf(" ");}
+         for(j = i; j>=1; j--)
+             {printf("%d",j);}
+         space -=2;
+         printf("\n");
+}
     return 0;
 }
-
