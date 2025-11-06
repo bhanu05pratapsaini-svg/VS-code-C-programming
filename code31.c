f4de07203faab55a5b99d9286017f23cd64e21a2
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-11-06 21:52:19 +0530

    DAY16 Q1

---
+++ code31.c
@@ -0,0 +1,26 @@
+// Q31: Write a program to take a number as input and print its equivalent binary representation.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int binary[32], i = 0;
+
+    if (n == 0) {
+        printf("0\n");
+        return 0;
+    }
+
+    while (n > 0) {
+        binary[i++] = n % 2;
+        n /= 2;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
