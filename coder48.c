3e18f6281a8b64839b5d0aed4a4a4ff6682b1ffb
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-11-21 12:21:24 +0530

    DAY24 Q2

---
+++ coder48.c
@@ -0,0 +1,15 @@
+// Q48: Write a program to print the  pattern
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {          
+        for (j = 1; j <= i; j++) {      
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
