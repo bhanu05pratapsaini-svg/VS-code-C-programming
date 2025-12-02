652c793a38cc17e9b89f688eb5dc0b27f5e9f78f
Bhanu Pratap <bhanu05pratapsaini@gmail.com>
2025-12-03 00:45:09 +0530

    DAY41 Q2

---
+++ code82.c
@@ -0,0 +1,14 @@
+// Q82: Print each character of a string on a new line.
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
